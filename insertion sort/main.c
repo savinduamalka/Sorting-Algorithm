7841d450d8579e3d9039d213661daed68663fe28
savinduamalka <savinduamalka@gmail.com>
2025-06-21 22:04:40 +0530

    implement insertion sort algorithm

--- insertion sort/main.c
+++ insertion sort/main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
 
 int main() {
     int arr[10] = {5, 2, 9, 1, 5, 6, 3, 8, 7, 4};
     int n = sizeof(arr) / sizeof(arr[0]);
-    
+
     return 0;
 }
