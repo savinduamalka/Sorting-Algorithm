2c1838129e60e8093ed48f24fd7718cddd33dd14
savinduamalka <savinduamalka@gmail.com>
2025-06-21 22:03:32 +0530

    add initial array setup

--- insertion sort/main.c
+++ insertion sort/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
+
 int main() {
-    printf("Hello, World!\n");
+    int arr[10] = {5, 2, 9, 1, 5, 6, 3, 8, 7, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
     return 0;
 }
