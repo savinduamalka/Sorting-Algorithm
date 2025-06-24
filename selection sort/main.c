ef375d5c98f076ec35d3981caed833161799d525
savinduamalka <savinduamalka@gmail.com>
2025-06-24 20:09:35 +0530

    implement selection sort algorithm

--- selection sort/main.c
+++ selection sort/main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+void selection_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }
+        int temp = arr[min_index];
+        arr[min_index] = arr[i];
+        arr[i] = temp;
+    }
+}
+
 int main() {
     printf("This is a selection sort program\n");
     return 0;
