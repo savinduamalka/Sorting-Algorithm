ad66030803978ad182d0eacbd331d7aa697b9d51
savinduamalka <savinduamalka@gmail.com>
2025-06-25 21:47:35 +0530

    add output for original array in main function

--- selection sort/main.c
+++ selection sort/main.c
@@ -20,7 +20,11 @@ int main() {
     int arr[] = {64, 25, 12, 22, 11};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-
-
+    printf("Original array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
