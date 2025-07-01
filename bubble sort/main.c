01421b438d2472588064135a3c625089e4bb294b
savinduamalka <savinduamalka@gmail.com>
2025-07-01 23:29:12 +0530

    add bubbleSort function implementation

--- bubble sort/main.c
+++ bubble sort/main.c
@@ -6,6 +6,16 @@ void swap(int *xp, int *yp) {
     *yp = temp;
 }
 
+void bubbleSort(int arr[], int n) {
+    int i, j;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1])
+                swap(&arr[j], &arr[j+1]);
+        }
+    }
+}
+
 int main() {
 
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
