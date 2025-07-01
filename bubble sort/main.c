0ea02cc4828de7718333abfa0081302209f1927d
savinduamalka <savinduamalka@gmail.com>
2025-07-01 23:28:50 +0530

    add swap function for Bubble Sort implementation

--- bubble sort/main.c
+++ bubble sort/main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
+void swap(int *xp, int *yp) {
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
 int main() {
 
-  int arr[] = {64, 34, 25, 12, 22, 11, 90};
-  int n = sizeof(arr) / sizeof(arr[0]);
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
 
     printf("Bubble Sort...\n");
+
     return 0;
 }
