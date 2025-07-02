5e6952704e92203ce95fc8b7fc8336491d137a95
savinduamalka <savinduamalka@gmail.com>
2025-07-02 23:55:08 +0530

    add printArray function to display sorted array

--- bubble sort/main.c
+++ bubble sort/main.c
@@ -16,6 +16,12 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
 int main() {
 
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
