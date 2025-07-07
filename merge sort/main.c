c538e8d6206fbfc9645e69fdafe206798ea06d45
savinduamalka <savinduamalka@gmail.com>
2025-07-07 20:36:19 +0530

    add array initialization and size calculation in main function

--- merge sort/main.c
+++ merge sort/main.c
@@ -2,5 +2,9 @@
 
 int main() {
     printf("Merge Sort program....\n");
+
+    int arr[] = {64, 34, 25, 12, 22, 11, 90, 88, 76, 50, 42};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
     return 0;
 }
