9597ec3e5dd86479951ef7d3a51ebfa510eeb1cb
savinduamalka <savinduamalka@gmail.com>
2025-07-08 22:11:55 +0530

    add printArray function to display array elements

--- merge sort/main.c
+++ merge sort/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+void printArray(int arr[], int size) {
+    
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
 int main() {
     printf("Merge Sort program....\n");
 
