770b0ca8a5e4b8b4c2592cac44acfa298e79edfb
savinduamalka <savinduamalka@gmail.com>
2025-07-09 22:07:30 +0530

    add merge function for Merge Sort implementation

--- merge sort/main.c
+++ merge sort/main.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
 
+void merge(int arr[], int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    i = 0; 
+    j = 0; 
+    k = left; 
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    
+    while (i < n1)
+        arr[k++] = L[i++];
+    while (j < n2)
+        arr[k++] = R[j++];
+}
+
 void printArray(int arr[], int size) {
     
     for (int i = 0; i < size; i++)
