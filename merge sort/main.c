2d5d42523014751a28e680e3cd42b09c936447ec
savinduamalka <savinduamalka@gmail.com>
2025-07-11 20:52:47 +0530

    print original array

--- merge sort/main.c
+++ merge sort/main.c
@@ -53,6 +53,10 @@ int main() {
 
     int arr[] = {64, 34, 25, 12, 22, 11, 90, 88, 76, 50, 42};
     int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+    
     
     return 0;
 }
